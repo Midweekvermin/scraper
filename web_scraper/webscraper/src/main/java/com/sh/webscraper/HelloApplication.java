ad7b01197737d75515eda48ef444f0b44f510eb3
midweekvermin <midweekvermin@gmail.com>
2024-11-20 13:18:31 -0600

    set up icons and background for application

--- web_scraper/webscraper/src/main/java/com/sh/webscraper/HelloApplication.java
+++ web_scraper/webscraper/src/main/java/com/sh/webscraper/HelloApplication.java
@@ -3,6 +3,7 @@ package com.sh.webscraper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,14 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 250);
         stage.setTitle("Scraper");
+        stage.getIcons().clear();
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_16x16.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_32x32.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_64x64.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_128x128.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icon_256x256.png")));
+
         stage.setScene(scene);
         stage.show();
     }
